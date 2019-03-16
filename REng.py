6cee6a42881c17d0dc0a51d11b049e273ddb9c49
shawnpaul123 <31085457+shawnpaul123@users.noreply.github.com>
2019-03-16 18:36:13 -0400

    Added tkinter functionality to display code

--- REng.py
+++ REng.py
@@ -9,8 +9,6 @@ A linear model does not output probabilities, but it treats the classes as numbe
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
-
-
 from pandas import Series, DataFrame
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
@@ -19,7 +17,8 @@ from sklearn.metrics import classification_report
 from tkinter import * 
 from tkinter.ttk import *
 
-dummy_values = False
+
+
 
 #Declaring variables
 #!Need to have sample descriptions - deal in tkkinter
@@ -29,7 +28,10 @@ user_3 = [[1,1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,1,1]]
 
 global df
 df = pd.read_csv('bank_full_w_dummy_vars.csv')
+global dummy_values
+dummy_values = False
 
+#preprocesing data <-> Dummy Variables Already exist in dataset!!!!
 def preprocess_data():
 	#preprocessing data - clearing missing values as we have almost 50,000 rows
 	df.isnull().sum()
@@ -44,17 +46,19 @@ def preprocess_data():
 		X = onehotencoder.fit_transform(X)
 		label_encoder = LabelEncoder()
 
-#!Dummy Variables Already exist in dataset!!!!
 
+#displays tkinter box
 def display_gui():
 	window = Tk() 
-	window.title("Welcome to LikeGeeks app")
+	window.title("Welcome to FirstWords")
 	window.geometry('350x200')
 	 
 	selected = IntVar() 
 	rad1 = Radiobutton(window,text='First', value=1, variable=selected) 
 	rad2 = Radiobutton(window,text='Second', value=2, variable=selected) 
 	rad3 = Radiobutton(window,text='Third', value=3, variable=selected)
+	def clicked():
+ 		print(selected.get())
 	 
 	 
 	btn = Button(window, text="Click Me", command=clicked) 
@@ -66,24 +70,55 @@ def display_gui():
 	window.mainloop()
 	return selected.get()
 
+def display_gui_2(report):
+	window = Tk()
+	window.title("Welcome to FirstWorld")
+	window.geometry('350x200')
+	def clicked():
+	 	messagebox.showinfo('Hi!', report)
+	btn = Button(window,text='Click here', command=clicked)
+	btn.grid(column=0,row=0)
+	window.mainloop()
 
 
 
-#!Need to have prediction function
 
 
+#main code!!!
 
-#main program
+#preprocess data
 preprocess_data()
+#get which user we want to test
+user_name = display_gui()#this is an integer
 
+#declaring feeatures I am going to look at
 X = df.ix[:,(18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36)].values
+#declaring features I am trying to predict
 y = df.ix[:,17].values
+
+#fiting data
 LogReg = LogisticRegression()
 LogReg.fit(X,y)
-test_pred = LogReg.predict(X)
-user_pred = LogReg.predict(user_1)
 
+#doing test on accuracy
+test_pred = LogReg.predict(X)
 report = classification_report(y,test_pred)
-print(report)
-print(user_pred)
+#predicting which user will do this
+if user_name == 1:
+	user_pred = LogReg.predict(user_1)
+if user_name == 2:
+	user_pred = LogReg.predict(user_2)
+if user_name == 3:
+	user_pred = LogReg.predict(user_3)
+
+
+display_gui_2(report)
+
+
+
+
+
+
+
+
 
