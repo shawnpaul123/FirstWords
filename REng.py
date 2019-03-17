893eee34f26a66808b7fa550099c223d5917c728
shawnpaul123 <31085457+shawnpaul123@users.noreply.github.com>
2019-03-16 21:22:20 -0400

    Made modifications to dataset

--- REng.py
+++ REng.py
@@ -1,11 +1,4 @@
-#!Recommendation Engine
-'''
-Using Logistic regression to predict the value of 
-categorical variables using predictor variables
-
-A linear model does not output probabilities, but it treats the classes as numbers (0 and 1) and fits the best hyperplane (for a single feature, it is a line) that minimizes the distances between the points and the hyperplane. So it simply interpolates between the points, and you cannot interpret it as probabilities.
-
-'''
+#!Recommendation Engine!
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
@@ -16,7 +9,8 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import classification_report
 from tkinter import * 
 from tkinter.ttk import *
-
+import threading
+from tkinter import messagebox
 
 
 
@@ -24,15 +18,15 @@ from tkinter.ttk import *
 #!Need to have sample descriptions - deal in tkkinter
 user_1 = [[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1]]
 user_2 = [[0,0,1,0,0,1,0,1,0,0,1,0,1,0,0,0,0,1,1]]
-user_3 = [[1,1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,1,1]]
+user_3 = [[1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
 global df
-df = pd.read_csv('bank_full_w_dummy_vars.csv')
+df = pd.read_csv('DatasetType.csv')
 global dummy_values
 dummy_values = False
 
 #preprocesing data <-> Dummy Variables Already exist in dataset!!!!
-def preprocess_data():
+def preprocess_data(type):
 	#preprocessing data - clearing missing values as we have almost 50,000 rows
 	df.isnull().sum()
 	print(df.shape)
@@ -48,9 +42,9 @@ def preprocess_data():
 
 
 #displays tkinter box
-def display_gui():
+def display_gui(type):
 	window = Tk() 
-	window.title("Welcome to FirstWords")
+	window.title("Choose one of 3 test Users")
 	window.geometry('350x200')
 	 
 	selected = IntVar() 
@@ -58,7 +52,8 @@ def display_gui():
 	rad2 = Radiobutton(window,text='Second', value=2, variable=selected) 
 	rad3 = Radiobutton(window,text='Third', value=3, variable=selected)
 	def clicked():
- 		print(selected.get())
+ 		messagebox.showinfo('FYI JUDGE!', "Just so you :) know each user has different attributes") 
+
 	 
 	 
 	btn = Button(window, text="Click Me", command=clicked) 
@@ -68,16 +63,28 @@ def display_gui():
 	btn.grid(column=3, row=0)
 	 
 	window.mainloop()
-	return selected.get()
+	global user_name
+	user_name =  selected.get()
 
-def display_gui_2(report):
+def display_gui_2(text_to_display):
 	window = Tk()
 	window.title("Welcome to FirstWorld")
 	window.geometry('350x200')
 	def clicked():
-	 	messagebox.showinfo('Hi!', report)
+	 	messagebox.showinfo('Accuracy Report', report)
 	btn = Button(window,text='Click here', command=clicked)
 	btn.grid(column=0,row=0)
+	window.mainloop()
+
+
+def display_gui_3(text_to_display):
+	window = Tk() 
+	window.title("Welcome to LikeGeeks app") 
+	window.geometry('350x200') 
+	def clicked(): 
+	    messagebox.showinfo('YourResult!', text_to_display) 
+	btn = Button(window,text='Click here', command=clicked) 
+	btn.grid(column=0,row=0) 
 	window.mainloop()
 
 
@@ -85,11 +92,23 @@ def display_gui_2(report):
 
 
 #main code!!!
+# Start new Threads to preprocess data and get inputs at the same time
+#We are using multithreading cause its fast and cool!
+thread1 = threading.Thread(target=display_gui, args=("",))
+thread2 = threading.Thread(target=preprocess_data, args=("",))
 
-#preprocess data
-preprocess_data()
-#get which user we want to test
-user_name = display_gui()#this is an integer
+
+# Start new Threads
+thread1.start()
+thread2.start()
+
+
+
+# Wait for all threads to complete
+thread1.join()
+thread2.join()
+
+
 
 #declaring feeatures I am going to look at
 X = df.ix[:,(18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36)].values
@@ -103,7 +122,10 @@ LogReg.fit(X,y)
 #doing test on accuracy
 test_pred = LogReg.predict(X)
 report = classification_report(y,test_pred)
+
+print (user_name)
 #predicting which user will do this
+
 if user_name == 1:
 	user_pred = LogReg.predict(user_1)
 if user_name == 2:
@@ -112,11 +134,17 @@ if user_name == 3:
 	user_pred = LogReg.predict(user_3)
 
 
+
+lol = user_pred[0]
+
+if lol == 0:
+	display_gui_3("This user will not be interested in this material")
+if lol == 1:
+	display_gui_3("This user will not be interested in this material")
+
+
+#Displays model acuracy report!!
 display_gui_2(report)
-
-
-
-
 
 
 
